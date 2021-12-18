652e44587373cd9357b869129f450ede26b7fb4a
NightGoat <latenightgoat@gmail.com>
2021-12-18 17:22:02 +0300

    expect initLogger fun

--- shared/src/androidMain/kotlin/ru/nightgoat/kmmflickr/Platform.kt
+++ shared/src/androidMain/kotlin/ru/nightgoat/kmmflickr/Platform.kt
@@ -1,5 +1,12 @@
 package ru.nightgoat.kmmflickr
 
+import io.github.aakira.napier.DebugAntilog
+import io.github.aakira.napier.Napier
+
 actual class Platform actual constructor() {
     actual val platform: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
+
+actual fun initLogger() {
+    Napier.base(DebugAntilog())
 }
--- shared/src/commonMain/kotlin/ru/nightgoat/kmmflickr/Platform.kt
+++ shared/src/commonMain/kotlin/ru/nightgoat/kmmflickr/Platform.kt
@@ -3,3 +3,5 @@ package ru.nightgoat.kmmflickr
 expect class Platform() {
     val platform: String
 }
+
+expect fun initLogger()

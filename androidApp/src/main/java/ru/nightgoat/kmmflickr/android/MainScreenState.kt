18bb8c37d4d422bf1fdf2dad298751bf936b0653
NightGoat <latenightgoat@gmail.com>
2021-12-18 13:16:34 +0300

    added Test state and changed in Error state throwable to String

--- androidApp/src/main/java/ru/nightgoat/kmmflickr/android/MainScreenState.kt
+++ androidApp/src/main/java/ru/nightgoat/kmmflickr/android/MainScreenState.kt
@@ -2,6 +2,7 @@ package ru.nightgoat.kmmflickr.android
 
 sealed class MainScreenState {
     object Loading : MainScreenState()
-    data class Error(val throwable: Throwable) : MainScreenState()
+    object Test : MainScreenState()
+    data class Error(val errorMessage: String) : MainScreenState()
     data class Images(val list: List<String> = emptyList()) : MainScreenState()
 }

979a9e979d1038ab4e1663303ab03c1c83119913
NightGoat <latenightgoat@gmail.com>
2021-12-18 13:20:47 +0300

    removed not used versions

--- buildSrc/src/main/kotlin/config/Versions.kt
+++ buildSrc/src/main/kotlin/config/Versions.kt
@@ -5,8 +5,6 @@ object Versions {
     const val gradle = "7.0.3"
     const val kotlin = "1.5.31"
 
-    const val navigation = "2.3.5"
-    const val lifecycle: String = "2.3.1"
     const val coroutinesAndroid = "1.5.1"
 
     /* test */
@@ -19,10 +17,6 @@ object Versions {
     const val composeViewModel = "2.4.0-beta01"
     const val coil = "1.4.0"
 
-    const val hilt = "2.38.1"
-    const val hiltWork = "1.0.0"
-    const val hiltCompiler = "1.0.0"
-    const val orbit = "4.2.0"
     const val accompanist = "0.20.2"
 
 }

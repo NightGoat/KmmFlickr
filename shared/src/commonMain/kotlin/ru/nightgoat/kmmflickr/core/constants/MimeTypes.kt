f1e38d5a41abcb63015e1f5e4b34bf26d2f168b1
NightGoat <latenightgoat@gmail.com>
2021-12-19 20:24:51 +0300

    added extensions field for MimeTypes

--- shared/src/commonMain/kotlin/ru/nightgoat/kmmflickr/core/constants/MimeTypes.kt
+++ shared/src/commonMain/kotlin/ru/nightgoat/kmmflickr/core/constants/MimeTypes.kt
@@ -1,11 +1,13 @@
 package ru.nightgoat.kmmflickr.core.constants
 
+import ru.nightgoat.kmmflickr.core.constants.MimeTypesConstants.JPEG_EXTENSION
 import ru.nightgoat.kmmflickr.core.constants.MimeTypesConstants.JPEG_MIME_VALUE
 
-enum class MimeTypes(val value: String) {
-    JPEG(JPEG_MIME_VALUE);
+enum class MimeTypes(val value: String, val extension: String) {
+    JPEG(JPEG_MIME_VALUE, JPEG_EXTENSION);
 }
 
 private object MimeTypesConstants {
     const val JPEG_MIME_VALUE = "image/jpeg"
+    const val JPEG_EXTENSION = ".jpg"
 }
